5c71db5c7f72baedb2ff7292c163edf42569d15c
Denis Churbanov <4urbanoff@gmail.com>
2021-07-24 13:29:43 +0300

    update faq

--- src/help.ts
+++ src/help.ts
@@ -7,10 +7,25 @@ export const helpController = (ctx: Context) => {
 }
 
 const createHelpResponse = () =>
-    `Axie Infinity Tools
+    `<pre>Axie Infinity Tools
 /rate - get current Binance prices
 /breed - get breed info
-/breed [father breed number][mother breed number][breed count] - get info about breed two Axies
+/breed [father][mother][eggs] - get info about breed two Axies
 
 Example:
-/breed 014 - breed info of: first Axie has 0 breed number, second Axie has 1 breed number and we need 4 eggs`
+/breed 014 - breed info of: first Axie has 0 breed number, second Axie has 1 breed number and we need 4 eggs
+
+Exchanges:
+/slp2usd 100
+/slp2axs 100
+/slp2eth 100
+/axs2usd 100
+/axs2slp 100
+/axs2eth 100
+/eth2usd 100
+/eth2slp 100
+/eth2axs 100
+/usd2slp 100
+/usd2axs 100
+/usd2eth 100
+</pre>`
