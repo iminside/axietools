2f93c08af0afa77519de2909971692f5fc05aa38
Denis Churbanov <4urbanoff@gmail.com>
2021-07-24 10:08:38 +0300

    update help info

--- src/help.ts
+++ src/help.ts
@@ -10,7 +10,7 @@ const createHelpResponse = () =>
     `Axie Infinity Tools
 /rate - get current Binance prices
 /breed - get breed info
-/breed [father breed number] [mother breed number] [breed count] - get info about breed two Axies
+/breed [father breed number][mother breed number][breed count] - get info about breed two Axies
 
 Example:
-/breed 0 1 4 - breed info of: first Axie has 0 breed number, second Axie has 1 breed number and we need 4 eggs`
+/breed 014 - breed info of: first Axie has 0 breed number, second Axie has 1 breed number and we need 4 eggs`
